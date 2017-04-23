a4eaac311d1853b3a6d492a8a5d6b863bdf3437d
rykeenan <ryan.keenan@udacity.com>
2017-04-23 15:00:39 -0700

    Update Notebook

--- code/perception.py
+++ code/perception.py
@@ -2,25 +2,10 @@ import numpy as np
 import cv2
 
 # Define a function to perform a perspective transform
-# I've used the example grid image above to choose source points for the
+# I've used the example_grid1.jpg image (in calibration images folder)
+# to choose source points for the
 # grid cell in front of the rover (each grid cell is 1 square meter in the sim)
-def perspect_transform(img):
-    
-    img_size = (img.shape[1], img.shape[0])
-    # Define calibration box in source (actual) and destination (desired) coordinates
-    # These source and destination points are defined to warp the image
-    # to a grid where each 10x10 pixel square represents 1 square meter
-    dst_size = 5 
-    # Set a bottom offset to account for the fact that the bottom of the image 
-    # is not the position of the rover but a bit in front of it
-    # this is just a rough guess, feel free to change it!
-    bottom_offset = 6
-    src = np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
-    dst = np.float32([[img_size[0]/2 - dst_size, img_size[1] - bottom_offset],
-                      [img_size[0]/2 + dst_size, img_size[1] - bottom_offset],
-                      [img_size[0]/2 + dst_size, img_size[1] - 2*dst_size - bottom_offset], 
-                      [img_size[0]/2 - dst_size, img_size[1] - 2*dst_size - bottom_offset],
-                      ])
+def perspect_transform(img, src, dst):
        
     M = cv2.getPerspectiveTransform(src, dst)
     warped = cv2.warpPerspective(img, M, img_size)# keep same size as input image
@@ -77,6 +62,7 @@ def pix_to_world(dist, angles, x_rover, y_rover, yaw_rover):
 # Apply the above functions in succession and update the Rover state accordingly
 def perception_step(Rover):
     # Perform perception steps to update Rover()
+    # Need to define source and destination points for perspective transform
     # Update Rover analysis image
         # Rover.vision_image = ??
     # Update Rover pixel distances and angles
