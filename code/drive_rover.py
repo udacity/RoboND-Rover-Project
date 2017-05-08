d106a22fbcdd7c6dd5aa50abf06069963e18b160
rykeenan <ryan.keenan@udacity.com>
2017-05-08 13:19:55 -0700

    Minor tweaks to send_control()

--- code/drive_rover.py
+++ code/drive_rover.py
@@ -166,8 +166,8 @@ def send_control(commands, image_string1, image_string2):
         'throttle': commands[0].__str__(),
         'brake': commands[1].__str__(),
         'steering_angle': commands[2].__str__(),
-        'inset_image': image_string1,
-        'inset_image3': image_string2,
+        'inset_image1': image_string1,
+        'inset_image2': image_string2,
         }
     # Send commands via socketIO server
     sio.emit(
