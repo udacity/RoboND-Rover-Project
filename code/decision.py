6a5776773afced2ec1014a1446d2d02fc6422968
Mihir D <mdeshingkar0@gmail.com>
2017-05-30 03:37:02 +0530

    stuck mode added. Additional features for navigating are added.Fidelity improved.

--- code/decision.py
+++ code/decision.py
@@ -24,8 +24,47 @@ def decision_step(Rover):
                 else: # Else coast
                     Rover.throttle = 0
                 Rover.brake = 0
+                
                 # Set steering to average angle clipped to the range +/- 15
-                Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
+                #Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi + 8), -15, 15)
+                if (np.any(Rover.nav_angles > 0)):
+                    nav_left = np.average(Rover.nav_angles * 180/np.pi, weights=(Rover.nav_angles > 0))
+                else:
+                    nav_left = -1
+                #nav_right = np.average(Rover.nav_angles * 180/np.pi, Rover.nav_angles < 0)
+                #wall_straight = np.average(Rover.obst_angles * 180/np.pi, (Rover.obst_angles < 2 and Rover.obst_angles > -2))
+                wall_left_dist = np.average(Rover.obst_dists * 180/np.pi, weights=(Rover.obst_angles > 0))
+                wall_right_dist = np.average(Rover.obst_dists * 180/np.pi, weights=(Rover.obst_angles < 0))
+                ########----------- Tested --------------#########
+                Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi + 8), -15, 15)
+                #wall_angle = np.mean(Rover.obst_angles * 180/np.pi)
+                #if wall_angle < 0.:
+                #    Rover.steer = 15
+                #if wall_angle > 6.:
+                #    Rover.steer = -15
+                #################################################
+                if(nav_left > 5 and ((wall_right_dist < wall_left_dist))): # and wall_left_dist > 1
+                    Rover.steer = 15
+                elif(nav_left < 5 and (nav_left != -1)):
+                    Rover.steer = -15
+                ###### Taking care of roll and pitch variations #####
+                if((Rover.roll > 1 and Rover.roll < 359) or (Rover.pitch > 0.5 and Rover.pitch < 0.5)):
+                    Rover.throttle = 0
+                    if Rover.vel < 0.2:
+                        Rover.steer = -15
+                        Rover.mode = 'stop'
+                    elif Rover.vel > 0.2:
+                        Rover.steer = 0
+                        Rover.brake = Rover.break_set
+                        Rover.mode = 'forward'
+                if(Rover.vel > 0):
+                    Rover.stuck_time = 0
+                elif (Rover.total_time > 1 and Rover.vel == 0.0):
+                    if Rover.stuck_time == 0:
+                        Rover.stuck_time = Rover.total_time
+                    elif (Rover.total_time - Rover.stuck_time) > 1:
+                        Rover.mode = 'stuck'
+
             # If there's a lack of navigable terrain pixels then go to 'stop' mode
             elif len(Rover.nav_angles) < Rover.stop_forward:
                     # Set mode to "stop" and hit the brakes!
@@ -59,13 +98,19 @@ def decision_step(Rover):
                     # Release the brake
                     Rover.brake = 0
                     # Set steer to mean angle
-                    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
+                    steer_nav = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
+                    Rover.steer = steer_nav
                     Rover.mode = 'forward'
+        elif Rover.mode == 'stuck':
+            Rover.throttle = 0
+            Rover.steer == -15
+            if(Rover.total_time - Rover.stuck_time) > 2:
+                Rover.mode = 'forward'
     # Just to make the rover do something 
     # even if no modifications have been made to the code
     else:
         Rover.throttle = Rover.throttle_set
-        Rover.steer = 0
+        Rover.steer = -15
         Rover.brake = 0
 
     return Rover
